f6a55bc72453850e69fa5a893c56b8277cc37953
Mathuniga <44501427+Mathuniga@users.noreply.github.com>
2018-11-18 00:54:03 -0500

    Layout changes

--- BI.php
+++ BI.php
@@ -7,60 +7,39 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<link rel="stylesheet" href="Portfolio.css">
-
+<link rel="stylesheet" href="spm.css">
 </head>
-<style>
 
-</style>
 <body>
 <div class="container">
-	<div class="bgBI">
-	<div class="w3-container w3-center w3-animate-zoom">
-		<div class="tab">
-          <button class="tablinks" onmouseover="opentab(event, 'BI')">Business Intelligence</button>
-          <button class="tablinks" onmouseover="opentab(event, 'BA')">Business Analytics</button>
-          <button class="tablinks" onmouseover="opentab(event, 'Data')">Data Integration Solutions</button>
-        </div>
-
+	<div class="bgICM">
+		<div class="main-box">
+				<img src="Images/ICMbg.jpg" alt="SPM" width="350" height="450">
+				<div class="centered">Business Intelligence</div>
+  				<div class="overlay">
+  					<p>BI supports corporate executives, business managers and other operational workers make better and more informed business decisions. Companies also use BI to cut costs, identify new business opportunities, and spot inefficient business processes ripe for re-engineering.</p>
+  				</div>
+  			
+		</div>
 		
-        <div id="BI" class="tabcontent">
-        <div class="w3-container w3-center w3-animate-zoom">
-        <?php 
-          echo "<h3>Business Intelligence</h3>";
-          echo "<p align=left><i>Yes, you hear the word buzz... here comes the Business intelligence!!!</i></p>";
-          echo "<p align=left><i>BI has come a long way - from assistance with report generation to self-service platforms for discovery and analytical insight.</i></p>";
-          echo "<p align=left><i>BI tools are essentially data-driven Decision Support Systems (DSS). BI is sometimes used interchangeably with briefing books, report and query tools and executive information systems.</i></p>";
-          echo "<p align=left><i>BI supports corporate executives, business managers and other operational workers which makes better and more informed business decisions. Companies also use BI to cut costs, identify new business opportunities, and spot inefficient business processes ripe for re-engineering.</i></p><br>";
-          echo "<p align=left><i>What We Do...</i></p>";
-          echo "<p align=left><i>We refer to the tools, technologies, applications and practices used to collect, integrate, analyze, and present an organization’s raw data in order to create insightful and actionable business information. We provide a technology-driven process is made up of several related activities including Data mining, online analytical processing, Querying and Reporting with high quality using tools like Power BI, Mule Soft and so on..</i></p>";
-         ?>
-        </div>
+		<div class="main-box">
+				<img src="Images/SPMImage.jpg" alt="ICM" width="350" height="450">
+				<div class="centered">Business Analytics</div>
+  				<div class="overlay">
+  					<p>Business analytics (BA) refers to skills, technologies, practices for continuous iterative exploration and investigation of past business performance to gain insight and drive business planning. Business analytics focuses on developing new insights and understanding of business performance based on data and Statistical methods.</p>
+  				</div>
 		</div>
-        <div id="BA" class="tabcontent">
-        <div class="w3-container w3-center w3-animate-zoom">
-          <?php 
-          echo "<h3>Business Analytics</h3>";
-          echo "<p align=left><i>Business analytics (BA) refers to skills, technologies, practices for continuous iterative exploration and investigation of past business performance to gain insight and drive business planning. Business analytics focuses on developing new insights and understanding of business performance based on data and Statistical methods.</i></p>";
-          echo "<p align=left><i>In other words, querying, reporting, OLAP known as BI, and alert tools can answer questions such as what happened, how many, how often, where the problem is, and what actions are needed. Business analytics can answer questions like why is this happening, what if these trends continue, what will happen next (predict), and what is the best outcome that can happen (optimize). </i></p><br>";
-          echo "<p align=left><i>What We Do...</i></p>";
-          echo "<p align=left><i>With BA we provide extensive use of data, statistical analysis, including explanatory and Predictive modeling and fact-based management to drive decision making. Analytics may be used as input for human decisions or may drive fully automated decisions. </i></p>";
-         ?> 
-        </div>
+		
+		<div class="main-box">
+				<img src="Images/BIFinal.jpg" alt="SPMvsICM" width="350" height="450">
+				<div class="centered">Data Integration</div>
+  				<div class="overlay">
+  					<p>Data integration is the combination of technical and business processes used to combine data from disparate sources into meaningful and valuable information. A complete data integration solution delivers trusted data from various sources.</p>
+  				</div>	
 		</div>
-        <div id="Data" class="tabcontent">
-        <div class="w3-container w3-center w3-animate-zoom">
-           <?php 
-          echo "<h3>Data Integration Solutions</h3>";
-          echo "<p align=left><i>Data integration is the combination of technical and business processes used to combine data from disparate sources into meaningful and valuable information. A complete data integration solution delivers trusted data from various sources.</i></p><br>";
-          echo "<p align=left><i>What We Do...</i></p>";
-          echo "<p align=left><i>We transform data in any style and deliver it to any system, supporting faster time to value and reduced IT risk. The integrated platform delivers a wide range of data quality capabilities from data profiling, standardization, matching and enrichment to active data-quality monitoring.</i></p>";
-         ?> 
-        </div>
-		</div>
-		<div class="clearfix"></div>
 	</div>
 </div>
-</div>
+
+
 </body>
 </html>
--- SPM.php
+++ SPM.php
@@ -1,4 +1,5 @@
-
+<!DOCTYPE html>
+<html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <script type="text/javascript" src="BI.js"></script>
@@ -12,55 +13,39 @@
 <body>
 <div class="container">
 	<div class="bgICM">
-	<div class="w3-container w3-center w3-animate-zoom">
-	<div class="tab">
-          <button class="tablinks" onmouseover="openspm(event, 'SPM')">Sales Performance Management</button>
-          <button class="tablinks" onmouseover="openspm(event, 'ICM')">Incentive Compensation Management</button>
-          <button class="tablinks" onmouseover="openspm(event, 'SPMvsICM')">SPM vs ICM</button>
-        </div>
-
-        <div id="SPM" class="tabcontent">
-        <div class="w3-container w3-center w3-animate-zoom">
-        <?php 
-          echo "<h1>Sales Performance Management</h1><br>";
-          echo "<p align=left><i>Sales performance management is a comprehensive solution that helps organizations drive sales alignment from planning through execution while improving efficiency, accuracy and timeliness of the associated administrative processes.</i> </p><br>";
-          echo "<p align=left><b><i>Motivate Sales</i></b></p>";
-          echo "<p align=left><i>Engage Salespeople with easy to read sales data using personalized dashboards and reports. Build trust with the sales team through transparent reporting showing salespeople their commissions and the calculation process. Provides the interface for controlling what your salespeople can see based on their role and eligibility rules.</i></p><br>";
-          echo "<p align=left><b><i>Automated Sales Comp</i></b></p>";
-          echo "<p align=left><i>Automate the jobs that other products can’t handle. Manage complex sales crediting rules. Take control of all your sales and commission data.SPM uses sophisticated plan rules and calculations and eliminate the manual labor required to make payments and manage the commission process.</i></p><br>";
-          echo "<p align=left><b><i>Dashboard & Reports</i></b></p>";
-          echo "<p align=left><i>SPM delivers immediate and transparent communication via dashboards and a suite of reports. Information can be customized with secure access levels for sales, human resources, finance and management.</i></p><br>";
-         ?>
-        </div>
+		<div class="main-box">
+				<img src="Images/ICMbg.jpg" alt="SPM" width="350" height="450">
+				<div class="centered">Sales Performance Management</div>
+  				<div class="overlay">
+  					<p>Sales performance management is a comprehensive solution that helps organizations drive 
+  					sales alignment from planning through execution while improving efficiency, accuracy and 
+  					timeliness of the associated administrative processes.</p>
+  				</div>
+  			
 		</div>
-        <div id="ICM" class="tabcontent">
-        <div class="w3-container w3-center w3-animate-zoom">
-          <?php 
-          echo "<h1>Incentive Compensation Management</h1>";
-          echo "<p align=left><i>Incentive compensation is the supplementary money an employee makes for a certain level of performance on top of their regular, base salary. Incentive compensation plan (or incentive pay plan) is a statement outlining the commission a sales rep receives when they sell a certain amount of a given product.</i></p>";
-          echo "<p align=left><b><i>Save Time</i></b>";
-          echo "<p align=left><i>Your time is too valuable to calculate sales commission manually with spreadsheets. With few clicks you can automate the process using IBM Incentive Compensation Management. The audit log to keep track of all the adjustments and you will receive an email confirming when the completion of the automated tasks.</i></p>";
-          echo "<p align=left><b><i>Ease of Use & Control</i></b>";
-          echo "<p align=left><i>Incentive Compensation Management gives you the power and authority to set up precisely what you need to have your commission calculated in detail. Incentive Compensation Management play nice with data from other programs like Salesforce, with our progressive ICM software.</i></p>";
-          echo "<p align=left><b><i>Integration</i></b>";
-          echo "<p align=left><i>IBM ICM uses several technologies including APIs to integrate with many other platforms including accounting systems like Salesforce, PeopleSoft. IBM ICM can interface with multiple sources of data to download and import files.</i></p>";
-          echo "<p align=left><b><i>Reports & Dashboard</i></p>";
-          echo "<p align=left><i>Use IBM ICM report and dashboard builder to create the reports your business needs. IBM ICM comes with a suite of standard reports, which you can easily edit or create your own reports.</i></p>";
-          ?> 
-        </div>
+		
+		<div class="main-box">
+				<img src="Images/SPMImage.jpg" alt="ICM" width="350" height="450">
+				<div class="centered">Incentive Compensation Management</div>
+  				<div class="overlay">
+  					<p>Incentive compensation is the supplementary money an employee makes for a certain level of performance on top of their regular, 
+  					base salary. Incentive compensation plan (or incentive pay plan) is a statement outlining the commission a sales rep receives 
+  					when they sell a certain amount of a given product. </p>
+  				</div>
 		</div>
-        <div id="SPMvsICM" class="tabcontent">
-        <div class="w3-container w3-center w3-animate-zoom">
-           <?php 
-          echo "<h1>SPM vs ICM</h1>";
-          echo "<p align=left><i>Sales performance management is a comprehensive solution that helps organizations drive sales alignment from planning through execution while improving efficiency, accuracy and timeliness of the associated administrative processes.</i></p><br>";
-          echo "<p align=left><i>Incentive compensation is the supplementary money an employee makes for a certain level of performance on top of their regular, base salary. Incentive compensation plan (or incentive pay plan) is a statement outlining the commission a sales rep receives when they sell a certain amount of a given product.</i></p><br>";
-          
-         ?> 
-        </div>
+		
+		<div class="main-box">
+				<img src="Images/BIFinal.jpg" alt="SPMvsICM" width="350" height="450">
+				<div class="centered">SPM vs ICM</div>
+  				<div class="overlay">
+  					<p>Incentive compensation is the supplementary money an employee makes for a certain level of performance on 
+  					top of their regular, base salary. Incentive compensation plan (or incentive pay plan) is a statement outlining the commission a sales rep receives
+  					when they sell a certain amount of a given product. </p>
+  				</div>	
 		</div>
-		<div class="clearfix"></div>
 	</div>
 </div>
-</div>
+
+
 </body>
+</html>
