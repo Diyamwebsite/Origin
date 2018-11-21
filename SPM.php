eedea036c25ba711f7f325188b9cf48b5c97b927
Mathuniga <44501427+Mathuniga@users.noreply.github.com>
2018-11-20 22:53:18 -0500

    Changes In-progress

--- SPM.php
+++ SPM.php
@@ -12,43 +12,45 @@
 
 <body>
 <div class="container">
-	<div class="bgICM">
-		<div class="main-box">
-				<img src="Images/ICMbg.jpg" alt="SPM" width="350" height="450">
-				<div class="centered">Sales Performance Management</div>
-  				<div class="overlay">
-  					<p>Sales performance management is a comprehensive solution that helps organizations drive 
-  					sales alignment from planning through execution while improving efficiency, accuracy and 
-  					timeliness of the associated administrative processes.</p>
-  					<div class="link"><a id="spm" href="spmcontent.php">Read More...</a></div>
-  				</div>
-		</div>
-		
-		<div class="main-box">
-				<img src="Images/ICM.jpg" alt="ICM" width="350" height="450">
-				<div class="centered">Incentive Compensation Management</div>
-  				<div class="overlay">
-  					<p>Incentive compensation is the supplementary money an employee makes for a certain level of performance on top of their regular, 
-  					base salary. Incentive compensation plan (or incentive pay plan) is a statement outlining the commission a sales rep receives 
-  					when they sell a certain amount of a given product. </p>
-  					<div class="link"><a id="ICM"  href="ICMContent.php">Read More...</a></div>
-  				</div>
-		</div>
-		
-		<div class="main-box">
-				<img src="Images/bg.jpg" alt="SPMvsICM" width="350" height="450">
-				<div class="centered">SPM vs ICM</div>
-  				<div class="overlay">
-  					<p>Incentive compensation is the supplementary money an employee makes for a certain level of performance on 
-  					top of their regular, base salary. Incentive compensation plan (or incentive pay plan) is a statement outlining the commission a sales rep receives
-  					when they sell a certain amount of a given product. </p>
-  					<div class="link"><a id="SPMICM"  href="SPMICMContent.php">Read More...</a></div>
-  				</div>	
-		</div>
+<div class="title">High Performing Sales and Service Strategies &amp; Solutions</div>
+<div class="card-deck mt-4">
+  <div class="card">
+    <img class="card-img-top" src="Images/Diyamlogo.png" alt="Card image cap" style="width: 75px; height: 75px;">
+    <div class="card-body">
+      <h3 class="card-title">SPM Service Implementation</h3>
+      <p class="card-text">Text here...</p>
+      <a href="#" class="btn btn-primary">Learn More</a>
+
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="Images/Diyamlogo.png" alt="Card image cap" style="width: 75px; height: 75px;">
+    <div class="card-body">
+      <h3 class="card-title">Support &amp; Maintenance</h3>
+      <p class="card-text">Text here...</p>
+      <a href="#" class="btn btn-primary">Learn More</a>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="Images/Diyamlogo.png" alt="Card image cap" style="width: 75px; height: 75px;">
+    <div class="card-body">
+      <h3 class="card-title">Testing Services</h3>
+      <p class="card-text">Text here...</p>
+      <a href="#" class="btn btn-primary">Learn More</a>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="Images/Diyamlogo.png" alt="Card image cap" style="width: 75px; height: 75px;">
+    <div class="card-body">
+      <h3 class="card-title">Consulting Services</h3>
+      <p class="card-text">Text here...</p>
+      <a href="#" class="btn btn-primary">Learn More</a>
+    </div>
+  </div>
+</div>
 
 		<iframe id="contentload" style="width:100%;height:100%; display:none" name="spm" ></iframe>
-		
-	</div>
+	
 </div>
 
 
