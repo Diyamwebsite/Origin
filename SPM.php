37a549a563e11eb6128dffb9d71d9ea43ce464ba
Mathuniga <44501427+Mathuniga@users.noreply.github.com>
2018-11-21 22:43:34 -0500

    Introduced Card Decks as per the recent discussion.

--- SPM.php
+++ SPM.php
@@ -14,38 +14,46 @@
 <div class="container">
 <div class="title">High Performing Sales and Service Strategies &amp; Solutions</div>
 <div class="card-deck mt-4">
+<div class="col-xs-6  col-sm-3 col-md-3">
   <div class="card">
-    <img class="card-img-top" src="Images/Diyamlogo.png" alt="Card image cap" style="width: 75px; height: 75px;">
+    <img class="img-fluid" src="Images/Implementation.png" alt="Card image cap" style="width: 75px; height: 75px;">
     <div class="card-body">
       <h3 class="card-title">SPM Service Implementation</h3>
-      <p class="card-text">Text here...</p>
-      <a href="#" class="btn btn-primary">Learn More</a>
+      <p class="card-text">Offering a complete end-to-end analysis for defining and planning a future state solution to support your SPM.</p>
+      <a href="spmcontent.php" class="btn btn-primary">Learn More</a>
 
     </div>
   </div>
+  </div>
+  <div class="col-xs-6  col-sm-3 col-md-3">
   <div class="card">
-    <img class="card-img-top" src="Images/Diyamlogo.png" alt="Card image cap" style="width: 75px; height: 75px;">
+    <img class="img-fluid" src="Images/SUPPORT.png" alt="Card image cap" style="width: 75px; height: 75px;">
     <div class="card-body">
       <h3 class="card-title">Support &amp; Maintenance</h3>
-      <p class="card-text">Text here...</p>
-      <a href="#" class="btn btn-primary">Learn More</a>
+      <p class="card-text">Delivering both business and technical support at the level that’s right for your organization.</p>
+      <br><a href="#" class="btn btn-primary">Learn More</a>
     </div>
   </div>
+  </div>
+  <div class="col-xs-6  col-sm-3 col-md-3">
   <div class="card">
-    <img class="card-img-top" src="Images/Diyamlogo.png" alt="Card image cap" style="width: 75px; height: 75px;">
+    <img class="img-fluid" src="Images/TESTING.png" alt="Card image cap" style="width: 75px; height: 75px;">
     <div class="card-body">
       <h3 class="card-title">Testing Services</h3>
       <p class="card-text">Text here...</p>
       <a href="#" class="btn btn-primary">Learn More</a>
     </div>
   </div>
+  </div>
+  <div class="col-xs-6  col-sm-3 col-md-3">
   <div class="card">
-    <img class="card-img-top" src="Images/Diyamlogo.png" alt="Card image cap" style="width: 75px; height: 75px;">
+    <img class="img-fluid" src="Images/CONSULTANT.png" alt="Card image cap" style="width: 75px; height: 75px;">
     <div class="card-body">
       <h3 class="card-title">Consulting Services</h3>
       <p class="card-text">Text here...</p>
       <a href="#" class="btn btn-primary">Learn More</a>
     </div>
+  </div>
   </div>
 </div>
 
